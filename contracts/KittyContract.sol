079d7f87228f3d3283a00d00cb586cbdfa54f7c4
Mark Watson <64367041+mjwatson10@users.noreply.github.com>
2020-09-01 16:59:39 -0700

    added functions to contract

--- contracts/KittyContract.sol
+++ contracts/KittyContract.sol
@@ -1,25 +1,31 @@
 pragma solidity ^0.5.12;
 
 //IECR721.sol is an interface (not a contract)
-import "./IECR721.sol";
+import "./IERC721.sol";
+import "./Owner.sol";
+import "./Safemath.sol";
 
-contract Kittycontract is IECR721 {
+contract Kittycontract is IERC721, Ownable {
 
-  string public constant name = "CyberKitties";
-  string public constant symbol = "CK";
 
-  struct Kitty {
-    uint256 genes;
-    uint64 birthTime;
-    uint32 momId;
-    uint32 dadId;
-    uint16 generation;
-  }
+        //this gets access the the library Safemath.sol
+        using SafeMath for uint256;
 
-  Kitty[] kitties;
+        string public constant _name = "CyberKitties";
+        string public constant _symbol = "CK";
 
-  mapping(uint256 => address) public kittyIndexToOwner;
-  mapping(address => uint256) ownershipTokenCount;
+        struct Kitty {
+          uint256 genes;
+          uint64 birthTime;
+          uint32 momId;
+          uint32 dadId;
+          uint16 generation;
+        }
+
+        Kitty[] kitties;
+
+        mapping(uint256 => address) public kittyIndexToOwner;
+        mapping(address => uint256) ownershipTokenCount;
 
 
   /**
@@ -40,7 +46,64 @@ contract Kittycontract is IECR721 {
    * @dev Returns the name of the token.
    */
   function name() external view returns (string memory tokenName){
-    return name;
+    return _name;
   }
+
+  /*
+   * @dev Returns the symbol of the token.
+   */
+  function symbol() external view returns (string memory tokenSymbol){
+    return _symbol;
+  }
+
+  /**
+   * @dev Returns the owner of the `tokenId` token.
+   *
+   * Requirements:
+   *
+   * - `tokenId` must exist.
+   */
+  function ownerOf(uint256 tokenId) external view returns (address owner){
+    require(tokenId < kitties.length);
+
+    return kittyIndexToOwner[tokenId];
+  }
+
+  /* @dev Transfers `tokenId` token from `msg.sender` to `to`.
+  *
+  *
+  * Requirements:
+  *
+  * - `to` cannot be the zero address.
+  * - `to` can not be the contract address.
+  * - `tokenId` token must be owned by `msg.sender`.
+  *
+  * Emits a {Transfer} event.
+  */
+ function transfer(address to, uint256 tokenId) external{
+   require(to != address(0));
+   //address(this) is the contract addres
+   require(to != address(this));
+   require(kittyIndexToOwner[tokenId] == msg.sender);
+
+   fromTransfer(msg.sender, to, tokenId);
+ }
+
+ //transfers token from original address
+ function fromTransfer(address _from, address _to, uint256 _tokenId) internal {
+   ownershipTokenCount[_to]++;
+
+   kittyIndexToOwner[_tokenId] = _to;
+
+   if (_from != address(0)) {
+     ownershipTokenCount[_from]--;
+   }
+
+   emit Transfer(_from, _to, _tokenId);
+ }
+
+
+
+
 
 }
