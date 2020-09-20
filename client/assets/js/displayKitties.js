a4f3db2171e4a8d883011d50f8bace7bc5009e3d
Mark Watson <64367041+mjwatson10@users.noreply.github.com>
2020-09-20 14:53:15 -0700

    update to functions

--- client/assets/js/displayKitties.js
+++ client/assets/js/displayKitties.js
@@ -20,14 +20,13 @@ async function ownersArray(){
 
 async function cardsData(){
   let ownedKitties = await ownersArray();
-  //let imgThumb = kittyThumbnail();
-  //let _dna = await spreadDna();
+  let imgThumb = kittyThumbnail();
+  let _dna = await spreadDna();
 
   let kittyCards = '';
   for (var i = 0; i < ownedKitties.length; i++){
         kittyCards += `<div class="card" style="width: 18rem;">
                               <div class="card-body">
-
                                 <div>${"DNA: " + ownedKitties[i].genes}</div>
                                 <div>${"Gen: " + ownedKitties[i].generation}</div>
                               </div>
@@ -79,51 +78,7 @@ function dnaOfKitty(dnaStr){
   return _dna;
 }
 
-/*
 
-
-function appendKitty(dna, id){
-  let _kittyDNA = dnaOfKitty(dna);
-
-  kittyThumbnail(id);
-
-  _renderCat(_kittyDNA, id);
-
-
-}
-
-function dnaOfKitty(dnaStr){
-  var _dna = {
-    "headcolor" : dnaStr.substring(0,2),
-    "legscolor" : dnaStr.substring(2,4),
-    "eyecolor" : dnaStr.substring(4,6),
-    "earcolor" : dnaStr.substring(6,8),
-    "pawcolor" : dnaStr.substring(8,10),
-    "bellycolor" : dnaStr.substring(10,12),
-    "eyesShape" : dnaStr.substring(12,13),
-    "decorationPattern" : dnaStr.substring(13,14),
-    "decorationTopcolor" : dnaStr.substring(14,16),
-    "decorationBottomcolor" : dnaStr.substring(16,18),
-    "animation" :  dnaStr.substring(18,19)
-  }
-  return _dna;
-}
-
-function _renderCat(dna, id){
-
-  headColor(dna.headcolor, id)
-  legsColor(dna.legscolor, id)
-  eyeColor(dna.eyecolor, id)
-  earColor(dna.earcolor, id)
-  pawColor(dna.pawcolor, id)
-  bellyColor(dna.bellycolor, id)
-  eyeVariation(dna.eyesShape, id)
-  decorationVariation(dna.decorationPattern, id)
-  decorationTopColor(dna.decorationTopcolor, id)
-  decorationBottomColor(dna.decorationBottomcolor, id)
-  _animationVariation(dna.animation, id)
-}
-*/
 function kittyThumbnail(){
   let kittyDiv = `<div class="row">
                     <div class="col-lg-4 catBox m-2 light-b-shadow">
