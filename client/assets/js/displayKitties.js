13faa3e352c1ababc0ae7cd601ffdf845dedfc09
Mark Watson <64367041+mjwatson10@users.noreply.github.com>
2020-09-25 16:06:25 -0700

    update

--- client/assets/js/displayKitties.js
+++ client/assets/js/displayKitties.js
@@ -112,49 +112,49 @@ function renderKitty(dna, id){
 }
 
 function _headColor(color,code, id) {
-    $(`#head ${id}, #body ${id}`).css('background', '#' + color)  //This changes the color of the cat
+    $(`#kitty${id} #head, #kitty${id} #body`).css('background', '#' + color)  //This changes the color of the cat
     $('#headcode').html('code: '+ code) //This updates text of the badge next to the slider
     $('#dnabody').html(code) //This updates the body color part of the DNA that is displayed below the cat
 }
 
 function _legsColor(color,code, id) {
-    $(`.front-leg ${id}, .back-leg-right ${id}, .back-leg-left ${id}, .tail ${id}`).css('color', '#' + color).css('background', '#' + color)
+    $(`#kitty${id} .front-leg, #kitty${id} .back-leg-right, #kitty${id} .back-leg-left, #kitty${id} .tail`).css('color', '#' + color).css('background', '#' + color)
     $('#legscode').html('code: '+ code)
     $('#dnalegs').html(code)
 }
 
 function _eyeColor(color,code, id) {
-    $(`.left-Eye ${id}, .right-Eye ${id}`).css('background', '#' + color)
+    $(`#kitty${id} .left-Eye, #kitty${id} .right-Eye`).css('background', '#' + color)
     $('#eyescode').html('code: '+code)
     $('#dnaeyes').html(code)
 }
 
 function _earColor(color,code, id) {
-    $(`.ear ${id}`).css('background', '#' + color)
+    $(`#kitty${id} .ear`).css('background', '#' + color)
     $('#earscode').html('code: '+code)
     $('#dnaears').html(code)
 }
 
 function _pawColor(color,code, id) {
-    $(`.inner-Ear ${id}, .paws ${id}, .tail-end ${id}, .paws-back-left ${id}, .paws-back-right ${id}`).css('background', '#' + color)
+    $(`#kitty${id} .inner-Ear, #kitty${id} .paws, #kitty${id} .tail-end, #kitty${id} .paws-back-left, #kitty${id} .paws-back-right`).css('background', '#' + color)
     $('#pawscode').html('code: '+code)
     $('#dnapaws').html(code)
 }
 
 function _bellyColor(color,code, id) {
-    $(`.lips ${id}, .tummy ${id}`).css('background', '#' + color)
+    $(`#kitty${id} .lips, #kitty${id} .tummy`).css('background', '#' + color)
     $('#bellycode').html('code: '+code)
     $('#dnabelly').html(code)
 }
 
 function _decorationTopColor(color,code, id) {
-    $(`.stripes-top-left ${id}, .stripes-top-right ${id}`).css('color', '#' + color)
+    $(`#kitty${id} .stripes-top-left, #kitty${id} .stripes-top-right`).css('color', '#' + color)
     $('#topcode').html('code: '+code)
     $('#dnadecorationTop').html(code)
 }
 
 function _decorationBottomColor(color,code, id) {
-    $(`.stripes-bottom-left ${id}, .stripes-bottom-right ${id}`).css('color', '#' + color)
+    $(`#kitty${id} .stripes-bottom-left, #kitty${id} .stripes-bottom-right`).css('color', '#' + color)
     $('#bottomcode').html('code: '+code)
     $('#dnadecorationBottom').html(code)
 }
@@ -294,85 +294,75 @@ function _animationVariation(num, id){
 
 
 function animationType1(id){
-  resetAnimation();
-  $(`#head ${id}`).addClass("movingHead");
-  $(`.ears ${id}`).addClass("movingEars");
+
+  $(`#kitty${id} #head`).addClass("movingHead");
+  $(`#kitty${id} .ears`).addClass("movingEars");
 }
 
 function animationType2(id){
-  resetAnimation();
-  $(`.nose ${id}`).addClass("wigglingNose");
+
+  $(`#kitty${id} .nose`).addClass("wigglingNose");
 }
 
 function animationType3(id){
-  resetAnimation();
-  $(`.eye-borders-left ${id}, .eye-borders-right ${id}`).addClass("blinkingEyes");
+
+  $(`#kitty${id} .eye-borders-left, #kitty${id} .eye-borders-right`).addClass("blinkingEyes");
 }
 
 function animationType4(id){
-  resetAnimation();
-  $(`.tail ${id}`).addClass("tailwagging");
-  $(`.tail-end ${id}`).addClass("tailendwagging");
+
+  $(`#kitty${id} .tail`).addClass("tailwagging");
+  $(`#kitty${id} .tail-end`).addClass("tailendwagging");
 }
 
 function animationType5(id){
-  resetAnimation();
-  $(`.ears ${id}`).addClass("earupdown");
+
+  $(`#kitty${id} .ears`).addClass("earupdown");
 }
 
 function animationType6(id){
-  resetAnimation();
-  $(`.left-Eye ${id}, .right-Eye ${id}`).addClass("eyesmoving");
-  $(`.pupils ${id}`).addClass("pupilsmoving");
-  $(`.eye-borders-left ${id}, .eye-borders-right ${id}`).addClass("blinkingEyes");
+
+  $(`#kitty${id} .left-Eye, #kitty${id} .right-Eye`).addClass("eyesmoving");
+  $(`#kitty${id} .pupils`).addClass("pupilsmoving");
+  $(`#kitty${id} .eye-borders-left, #kitty${id} .eye-borders-right`).addClass("blinkingEyes");
 }
 
-function resetAnimation(){
-  $("#head").removeClass("movingHead");
-  $(".ears").removeClass("movingEars");
-  $(".nose").removeClass("wigglingNose");
-  $(".eye-borders-left, .eye-borders-right").removeClass("blinkingEyes");
-  $(".tail").removeClass("tailwagging");
-  $(".tail-end").removeClass("tailendwagging");
-  $(".ears").removeClass("earupdown");
-  $(".left-Eye, .right-Eye").removeClass("eyesmoving");
-  $(".pupils").removeClass("pupilsmoving");
-}
+
 
 function normalEyes(id) {
-    $(`.eye-borders-left ${id}, .eye-borders-right ${id}`).css('border', 'none')
+    $(`#kitty${id} .eye-borders-left, #kitty${id} .eye-borders-right`).css('border', 'none')
 }
 
 function eyesType1(id){
-    $(`.eye-borders-left ${id}, .eye-borders-right ${id}`).css('border-top', '15px solid')
+    $(`#kitty${id} .eye-borders-left, #kitty${id} .eye-borders-right`).css('border-top', '15px solid')
 }
 
 function eyesType2(id){
-    $(`.eye-borders-left ${id}, .eye-borders-right ${id}`).css('border-top', '30px solid')
+    $(`#kitty${id} .eye-borders-left, #kitty${id} .eye-borders-right`).css('border-top', '30px solid')
 }
 
 function eyesType3(id){
-    $(`.eye-borders-left ${id}, .eye-borders-right ${id}`).css('border-top', '45px solid')
+    $(`#kitty${id} .eye-borders-left, #kitty${id} .eye-borders-right`).css('border-top', '45px solid')
 }
 
 function eyesType4(id){
-    $(`.eye-borders-left ${id}, .eye-borders-right ${id}`).css('border-top', '55px solid')
+    $(`#kitty${id} .eye-borders-left, #kitty${id} .eye-borders-right`).css('border-top', '55px solid')
 }
 
 function eyesType5(id){
-    $(`.eye-borders-left ${id}, .eye-borders-right ${id}`).css('border-bottom', '15px solid')
+    $(`#kitty${id} .eye-borders-left, #kitty${id} .eye-borders-right`).css('border-bottom', '15px solid')
 }
 
 function eyesType6(id){
-    $(`.eye-borders-left ${id}, .eye-borders-right ${id}`).css('border-bottom', '30px solid')
+    $(`#kitty${id} .eye-borders-left, #kitty${id} .eye-borders-right`).css('border-bottom', '30px solid')
 }
 
 function eyesType7(id){
-    $(`.eye-borders-right ${id}`).css('border-top', '35px solid')
+    $(`#kitty${id} .eye-borders-right`).css('border-top', '35px solid')
 }
 
 function eyesType8(id){
-    $(`.eye-borders-left ${id}`).css('border-top', '35px solid')
+    $(`#kitty${id} .eye-borders-left`).css('border-top', '35px solid')
 }
 
 
@@ -380,62 +370,62 @@ function eyesType8(id){
 function normaldecoration() {
     //Remove all style from other decorations
     //In this way we can also use normalDecoration() to reset the decoration style
-    $(`.stripes-top-left`).css({ "top": "-55px" }).css({ "left": "70px" })
-    $(`.stripes-top-right`).css({ "top": "-20px" }).css({ "left": "48px" })
-    $(`.stripes-bottom-left`).css({ "top": "37px" }).css({ "left": "-26px" })
-    $(`.stripes-bottom-right`).css({ "top": "150px" }).css({ "left": "118px" })
+    $(`#kitty${id} .stripes-top-left`).css({ "top": "-55px" }).css({ "left": "70px" })
+    $(`#kitty${id} .stripes-top-right`).css({ "top": "-20px" }).css({ "left": "48px" })
+    $(`#kitty${id} .stripes-bottom-left`).css({ "top": "37px" }).css({ "left": "-26px" })
+    $(`#kitty${id} .stripes-bottom-right`).css({ "top": "150px" }).css({ "left": "118px" })
 }
 
 function stripeType1(id){
-    $(`.stripes-top-left ${id}`).css({ "top": "-55px" }).css({ "left": "60" })
-    $(`.stripes-top-right ${id}`).css({ "top": "-20px" }).css({ "left": "58px" })
+    $(`#kitty${id} .stripes-top-left`).css({ "top": "-55px" }).css({ "left": "60" })
+    $(`#kitty${id} .stripes-top-right`).css({ "top": "-20px" }).css({ "left": "58px" })
 }
 
 function stripeType2(id){
-    $(`.stripes-top-left ${id}`).css({ "top": "-55px" }).css({ "left": "60" })
-    $(`.stripes-top-right ${id}`).css({ "top": "-20px" }).css({ "left": "58px" })
-    $(`.stripes-bottom-left ${id}`).css({ "top": "42px" }).css({ "left": "-26px" })
-    $(`.stripes-bottom-right ${id}`).css({ "top": "155px" }).css({ "left": "118px" })
+    $(`#kitty${id} .stripes-top-left`).css({ "top": "-55px" }).css({ "left": "60" })
+    $(`#kitty${id} .stripes-top-right`).css({ "top": "-20px" }).css({ "left": "58px" })
+    $(`#kitty${id} .stripes-bottom-left`).css({ "top": "42px" }).css({ "left": "-26px" })
+    $(`#kitty${id} .stripes-bottom-right`).css({ "top": "155px" }).css({ "left": "118px" })
 }
 
 function stripeType3(id){
-    $(`.stripes-top-left ${id}`).css({ "top": "-55px" }).css({ "left": "73" })
-    $(`.stripes-top-right ${id}`).css({ "top": "-20px" }).css({ "left": "45px" })
-    $(`.stripes-bottom-left ${id}`).css({ "top": "42px" }).css({ "left": "-26px" })
-    $(`.stripes-bottom-right ${id}`).css({ "top": "155px" }).css({ "left": "118px" })
+    $(`#kitty${id} .stripes-top-left`).css({ "top": "-55px" }).css({ "left": "73" })
+    $(`#kitty${id} .stripes-top-right`).css({ "top": "-20px" }).css({ "left": "45px" })
+    $(`#kitty${id} .stripes-bottom-left`).css({ "top": "42px" }).css({ "left": "-26px" })
+    $(`#kitty${id} .stripes-bottom-right`).css({ "top": "155px" }).css({ "left": "118px" })
 }
 
 function stripeType4(id){
-    $(`.stripes-top-left ${id}`).css({ "top": "-55px" }).css({ "left": "60" })
-    $(`.stripes-top-right ${id}`).css({ "top": "-20px" }).css({ "left": "58px" })
-    $(`.stripes-bottom-left ${id}`).css({ "top": "42px" }).css({ "left": "-16" })
-    $(`.stripes-bottom-right ${id}`).css({ "top": "155px" }).css({ "left": "108" })
+    $(`#kitty${id} .stripes-top-left`).css({ "top": "-55px" }).css({ "left": "60" })
+    $(`#kitty${id} .stripes-top-right`).css({ "top": "-20px" }).css({ "left": "58px" })
+    $(`#kitty${id} .stripes-bottom-left`).css({ "top": "42px" }).css({ "left": "-16" })
+    $(`#kitty${id} .stripes-bottom-right`).css({ "top": "155px" }).css({ "left": "108" })
 }
 
 function stripeType5(id){
-    $(`.stripes-top-left ${id}`).css({ "top": "-55px" }).css({ "left": "60" })
-    $(`.stripes-top-right ${id}`).css({ "top": "-20px" }).css({ "left": "58px" })
-    $(`.stripes-bottom-left ${id}`).css({ "top": "32" }).css({ "left": "-16" })
-    $(`.stripes-bottom-right ${id}`).css({ "top": "145" }).css({ "left": "108" })
+    $(`#kitty${id} .stripes-top-left`).css({ "top": "-55px" }).css({ "left": "60" })
+    $(`#kitty${id} .stripes-top-right`).css({ "top": "-20px" }).css({ "left": "58px" })
+    $(`#kitty${id} .stripes-bottom-left`).css({ "top": "32" }).css({ "left": "-16" })
+    $(`#kitty${id} .stripes-bottom-right`).css({ "top": "145" }).css({ "left": "108" })
 }
 
 function stripeType6(id){
-    $(`.stripes-top-left ${id}`).css({ "top": "-55px" }).css({ "left": "60" })
-    $(`.stripes-top-right ${id}`).css({ "top": "-20px" }).css({ "left": "58px" })
-    $(`.stripes-bottom-left ${id}`).css({ "top": "52px" }).css({ "left": "-16" })
-    $(`.stripes-bottom-right ${id}`).css({ "top": "165px" }).css({ "left": "108" })
+    $(`#kitty${id} .stripes-top-left`).css({ "top": "-55px" }).css({ "left": "60" })
+    $(`#kitty${id} .stripes-top-right`).css({ "top": "-20px" }).css({ "left": "58px" })
+    $(`#kitty${id} .stripes-bottom-left`).css({ "top": "52px" }).css({ "left": "-16" })
+    $(`#kitty${id} .stripes-bottom-right`).css({ "top": "165px" }).css({ "left": "108" })
 }
 
 function stripeType7(id){
-    $(`.stripes-top-left ${id}`).css({ "top": "-55px" }).css({ "left": "70" })
-    $(`.stripes-top-right ${id}`).css({ "top": "-20px" }).css({ "left": "48px" })
-    $(`.stripes-bottom-left ${id}`).css({ "top": "52px" }).css({ "left": "-16" })
-    $(`.stripes-bottom-right ${id}`).css({ "top": "165px" }).css({ "left": "108" })
+    $(`#kitty${id} .stripes-top-left`).css({ "top": "-55px" }).css({ "left": "70" })
+    $(`#kitty${id} .stripes-top-right`).css({ "top": "-20px" }).css({ "left": "48px" })
+    $(`#kitty${id} .stripes-bottom-left`).css({ "top": "52px" }).css({ "left": "-16" })
+    $(`#kitty${id} .stripes-bottom-right`).css({ "top": "165px" }).css({ "left": "108" })
 }
 
 function stripeType8(id){
-    $(`.stripes-top-left ${id}`).css({ "top": "-32px" }).css({ "left": "60" })
-    $(`.stripes-top-right ${id}`).css({ "top": "3px" }).css({ "left": "58px" })
-    $(`.stripes-bottom-left ${id}`).css({ "top": "32" }).css({ "left": "-16" })
-    $(`.stripes-bottom-right ${id}`).css({ "top": "145" }).css({ "left": "108" })
+    $(`#kitty${id} .stripes-top-left`).css({ "top": "-32px" }).css({ "left": "60" })
+    $(`#kitty${id} .stripes-top-right`).css({ "top": "3px" }).css({ "left": "58px" })
+    $(`#kitty${id} .stripes-bottom-left`).css({ "top": "32" }).css({ "left": "-16" })
+    $(`#kitty${id} .stripes-bottom-right`).css({ "top": "145" }).css({ "left": "108" })
 }
--- client/main.js
+++ client/main.js
@@ -21,52 +21,52 @@ function kittyThumbnail(id){
 
   let kittyDiv = `<div class="row">
                     <div class="col-lg-4 catBox m-2 light-b-shadow">
-                      <div class="cat">
-                      <div class="tail-end ${id}"></div>
-                      <div class="tail ${id}">J</div>
+                      <div class="cat" id="kitty${id}">
+                      <div class="tail-end"></div>
+                      <div class="tail">J</div>
                         <div class="ears earupdown">
-                                <div class="ear ${id}" id="left-ear">
-                                    <div class="inner-Ear ${id}" id="inner-left-ear"></div>
+                                <div class="ear" id="left-ear">
+                                    <div class="inner-Ear" id="inner-left-ear"></div>
                                 </div>
-                                <div class="ear ${id}" id="right-ear">
-                                    <div class="inner-Ear ${id}" id="inner-right-ear"></div>
+                                <div class="ear" id="right-ear">
+                                    <div class="inner-Ear" id="inner-right-ear"></div>
                                 </div>
                         </div>
-                        <div id="body ${id}">
-                                <div class="tummy ${id}"></div>
+                        <div id="body">
+                                <div class="tummy"></div>
                                 <div class="back-legs">
-                                    <div class="back-leg-left ${id}">
-                                        <div class="paws-back-left ${id}"></div>
+                                    <div class="back-leg-left">
+                                        <div class="paws-back-left"></div>
                                     </div>
-                                    <div class="back-leg-right ${id}">
-                                        <div class="paws-back-right ${id}"></div>
+                                    <div class="back-leg-right">
+                                        <div class="paws-back-right"></div>
                                     </div>
                                 </div>
                                 <div class="front-legs">
-                                    <div class="front-leg ${id}">
-                                        <div class="paws ${id}"></div>
+                                    <div class="front-leg">
+                                        <div class="paws"></div>
                                     </div>
-                                    <div class="front-leg ${id}">
-                                        <div class="paws ${id}"></div>
+                                    <div class="front-leg">
+                                        <div class="paws"></div>
                                     </div>
                                 </div>
                           </div>
-                          <div id="head ${id}">
+                          <div id="head">
                               <div class="stripes">
-                                <div class="stripes-top-left ${id}">~</div>
-                                <div class="stripes-top-right ${id}">~</div>
-                                <div class="stripes-bottom-left ${id}">~</div>
-                                <div class="stripes-bottom-right ${id}">~</div>
+                                <div class="stripes-top-left">~</div>
+                                <div class="stripes-top-right">~</div>
+                                <div class="stripes-bottom-left">~</div>
+                                <div class="stripes-bottom-right">~</div>
                               </div>
                                 <div class="eyes">
                                     <div class="eye">
-                                        <div class="left-Eye ${id} eyesmoving">
+                                        <div class="left-Eye eyesmoving">
                                             <div class="pupils pupilsmoving"></div>
                                             <div class="eye-borders-left"></div>
                                         </div>
                                     </div>
                                     <div class="eye">
-                                        <div class="right-Eye ${id} eyesmoving">
+                                        <div class="right-Eye eyesmoving">
                                             <div class="pupils pupilsmoving"></div>
                                             <div class="eye-borders-right"></div>
                                         </div>
@@ -74,10 +74,10 @@ function kittyThumbnail(id){
                                 </div>
                                 <div class="mouth">
                                     <div class="jaw"></div>
-                                    <div class="lips ${id}">
+                                    <div class="lips">
                                         <div class="whiskers" id="right-whisker">///</div>
                                     </div>
-                                    <div class="lips ${id}">
+                                    <div class="lips">
                                         <div class="whiskers" id="left-whisker">///</div>
                                     </div>
                                 </div>
@@ -86,6 +86,6 @@ function kittyThumbnail(id){
                                     <div class="right-nose"></div>
                                 </div>
                             </div>
-                          </div>`
+                          </div>`;
                   return kittyDiv;
 }
