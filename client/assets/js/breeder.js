4a1786045dc3549ad80016a41c404fbb4db8585e
Mark Watson <64367041+mjwatson10@users.noreply.github.com>
2020-10-14 13:47:54 -0700

    test

--- client/assets/js/breeder.js
+++ client/assets/js/breeder.js
@@ -23,8 +23,7 @@ $('#myModal').on('shown.bs.modal', function () {
 });*/
 
 //Parents Buttons
-$(document).ready(async function(){
-  $('#saveButton').click(async function(){
+/*$('#saveButton').click(async function(){
     let result = $('input[type= "checkbox"]:checked');
 
     if (result.length > 0 && result.length < 2) {
@@ -35,42 +34,42 @@ $(document).ready(async function(){
     } else {
       alert("Please choose one Daddy");
     }
+  });*/
+
+
+  $(document).ready(async function(){
+    $('#saveButton').click(async function(){
+      let result = $('input[type= "checkbox"]:checked');
+
+      if (result.length > 0 && result.length < 2) {
+
+        $('.momDisplay').html(result.val() + `<br> <p>Mommy</p>`);
+        $('#saveButton').attr("data-dismiss","modal");
+        console.log("mommy");
+      } else {
+        alert("Please choose one Daddy");
+      }
+    });
   });
-});
-
-$(document).ready(async function(){
-  $('#saveButton').click(async function(){
-    let result = $('input[type= "checkbox"]:checked');
-
-    if (result.length > 0 && result.length < 2) {
-      let _mom = await chosenParent(result.val());
-      $('.momDisplay').html(_mom + `<br> <p>Mommy</p>`);
-      $('#saveButton').attr("data-dismiss","modal");
-      console.log("mommy");
-    } else {
-      alert("Please choose one Daddy");
-    }
-  });
-});
 
 
-async function chosenParent(value){
-  let _ownedKitties = await ownersArray();
-  let _birth = await birthArray();
+  async function chosenParent(value){
+    let _ownedKitties = await ownersArray();
+    let _birth = await birthArray();
 
-  let _imgThumb = await kittyThumbnail(value);
-  let _dna = await dnaOfKitty(_ownedKitties[value]);
+    let _imgThumb = await kittyThumbnail(value);
+    let _dna = await dnaOfKitty(_ownedKitties[value]);
 
-chosenParent = `<div id="parentKitty">${_imgThumb}
-                    <br>
-                   <div class="catGenes">${"DNA: " + _birth[value].genes}</div>
-                    <br>
-                   <div class="catGenes">${"Gen: " + _birth[value].generation}</div>
-                </div>`
+  chosenParent = `<div id="parentKitty">${_imgThumb}
+                      <br>
+                     <div class="catGenes">${"DNA: " + _birth[value].genes}</div>
+                      <br>
+                     <div class="catGenes">${"Gen: " + _birth[value].generation}</div>
+                  </div>`
 
-      renderKitty(_dna, value)
-  return chosenParent;
-}
+        renderKitty(_dna, value)
+    return chosenParent;
+  }
 
 
 async function parentData(){
@@ -84,7 +83,7 @@ async function parentData(){
 
         let kittyCards = `<div class="col-lg-4 catParent">
                             <label class="option_item">
-                              <input type="checkbox" class="checkbox" id="kittyParent" value=${i}>
+                              <input type="checkbox" class="checkbox" id="kittyParent" value=${birth[i].genes}>
                                 <div class="option_inner">
                                         <div class="cards parent-cards">
                                               <div class="card-body parent-card">
@@ -100,7 +99,7 @@ async function parentData(){
                                   </div>
                               </label>
                             </div>`
-            $("#parents").append(kittyCards);
+            $(".parents").append(kittyCards);
             renderKitty(_dna, i);
         }
 
