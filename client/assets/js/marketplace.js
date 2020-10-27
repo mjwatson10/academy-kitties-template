5e3731626401c156706c15fa25731d86c3943c2e
Mark Watson <64367041+mjwatson10@users.noreply.github.com>
2020-10-27 14:43:58 -0700

    sell kitty update

--- client/assets/js/marketplace.js
+++ client/assets/js/marketplace.js
@@ -74,93 +74,36 @@ async function sellKitty(price, id){
 }
 
 
-$("#btn2").click(function(){
-  //  let price = $("#currency-field").val();
+$("#submitPrice").click(async function(){
+  let setPrice = $("#price-field").val();
 
-    console.log("hello");
+  await sellKitty(setPrice, sellerId);
+  await sellingKitties(kittyForSale, allKittiesBeingSold, setPrice);
+
+
+    alert(setPrice);
+    console.log(sellerId);
 })
 
 
-//currency formatting
-$("input[data-type='currency']").on({
-    keyup: function() {
-      formatCurrency($(this));
-    },
-    blur: function() {
-      formatCurrency($(this), "blur");
-    }
-});
+async function sellingKitties(value, placement, price){
+  let _ownedKitties = await ownersArray();
+  let _birth = await birthArray();
 
+  let _imgThumb = await kittyThumbnail(value);
+  let _dna = await dnaOfKitty(_ownedKitties[value]);
 
-function formatNumber(n) {
-  // format number 1000000 to 1,234,567
-  return n.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-}
+displayParent = `<div id="parentChosen">${_imgThumb}
+                    <br>
+                   <div class="catGenes">${"DNA: " + _birth[value].genes}</div>
+                    <br>
+                   <div class="catGenes">${"Gen: " + _birth[value].generation}</div>
+                    <br>
+                   <div class="catGenes">${"Price: " + price}</div>
+                    <br>
+                   <button id="buyKittyBtn">Buy</button>
+                </div>`
 
-
-function formatCurrency(input, blur) {
-  // appends $ to value, validates decimal side
-  // and puts cursor back in right position.
-
-  // get input value
-  var input_val = input.val();
-
-  // don't validate empty input
-  if (input_val === "") { return; }
-
-  // original length
-  var original_len = input_val.length;
-
-  // initial caret position
-  var caret_pos = input.prop("selectionStart");
-
-  // check for decimal
-  if (input_val.indexOf(".") >= 0) {
-
-    // get position of first decimal
-    // this prevents multiple decimals from
-    // being entered
-    var decimal_pos = input_val.indexOf(".");
-
-    // split number by decimal point
-    var left_side = input_val.substring(0, decimal_pos);
-    var right_side = input_val.substring(decimal_pos);
-
-    // add commas to left side of number
-    left_side = formatNumber(left_side);
-
-    // validate right side
-    right_side = formatNumber(right_side);
-
-    // On blur make sure 2 numbers after decimal
-    if (blur === "blur") {
-      right_side += "00";
-    }
-
-    // Limit decimal to only 2 digits
-    right_side = right_side.substring(0, 2);
-
-    // join number by .
-    input_val = "$" + left_side + "." + right_side;
-
-  } else {
-    // no decimal entered
-    // add commas to number
-    // remove all non-digits
-    input_val = formatNumber(input_val);
-    input_val = "$" + input_val;
-
-    // final formatting
-    if (blur === "blur") {
-      input_val += ".00";
-    }
-  }
-
-  // send updated string to input
-  input.val(input_val);
-
-  // put caret back in the right position
-  var updated_len = input_val.length;
-  caret_pos = updated_len - original_len + caret_pos;
-  input[0].setSelectionRange(caret_pos, caret_pos);
+      $(placement).append(displayParent);
+      renderKitty(_dna, value);
 }
